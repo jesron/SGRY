0cd4d21a3a067e6ac55d6754cbb11a97e55a4461
SuperRookieMam <dbk5k5k5>
2020-02-25 09:44:11 +0800

    log被忽略了哈哈

---
+++ SGRY.Api/RequestPayload/Rbac/Log/DncLogRequestPayload.cs
@@ -0,0 +1,26 @@
+﻿
+using static SGRY.Api.Entities.Enums.CommonEnum;
+
+namespace SGRY.Api.RequestPayload.Rbac.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DncLogRequestPayload : RequestPayload
+    {
+        /// <summary>
+        /// 是否已被删除
+        /// </summary>
+        public IsDeleted IsDeleted { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public Status Status { get; set; }  
+    }
+}
+
+
+
+
+
+
